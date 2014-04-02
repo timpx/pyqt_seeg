5362011fe07058da07adfc658f21c97162f0d8f7
tim <tim@timmy>
2014-04-02 16:06:14 +0200

    I forgot I add a repo there

--- pyqt_local_visualizer.py
+++ pyqt_local_visualizer.py
@@ -7,40 +7,45 @@ from scipy.signal import butter, lfilter
 import os
 
 print('load_data')
-path = '/disk2/Work/Simulations/TVB/'
-## Surface
-verts = np.load(os.path.join(path, 'pyqt_data/verts.npy'))
-faces = np.load(os.path.join(path, 'pyqt_data/faces.npy'))
+path = '/home/tim/Work/Simulations/TVB/'
+
+# Surface
+verts = np.loadtxt(os.path.join(path, 'pyqt_data/vertices.txt'))
+faces = np.loadtxt(os.path.join(path, 'pyqt_data/triangles.txt'))
+faces = faces.astype(int)
 centres = np.loadtxt(os.path.join(path, 'pyqt_data/centres.txt'))
-vertex_mapping = np.load(os.path.join(path, 'pyqt_data/vertex_mapping.npy')  )
-g = open(os.path.join(path, 'pyqt_data/name_regions.txt'),'r')
+vertex_mapping = np.load(os.path.join(path, 'pyqt_data/vertex_mapping.npy'))
+g = open(os.path.join(path, 'pyqt_data/name_regions.txt'), 'r')
 global name_regions
 name_regions = []
 for line in g:
     name_regions.append(line)
 g.close()
 
-## TAVG
+# TAVG
 x01_curr = 2.5
 x02_curr = 3.0
 num_curr = 1.0
 Ks_curr = 0.1
 x0_curr = 38
 global tavgs
-tavgs = np.load(os.path.join(path, 'sEEG_sim/results/compressed_different_'+str(x02_curr)+
-                '_epileptogenic_'+str(x01_curr)+'_net_'
-                +str(num_curr)+'_Ks_'+str(Ks_curr)+'_x0_'+str(x0_curr)+'.npy'))
+#print os.getcwd()
+tavgs = np.load('../TVB/my_tvb_data/simulations/TAVG_5.npy')
+tavgs = (np.squeeze(tavgs)).transpose()
+# projection matrix and seegs
+# proj_mat = np.load(os.path.join(path, 'pyqt_data/seeg_projection_matrix.npy'))
+# seegs_not_filtered = np.dot(proj_mat, tavgs)
+seegs_not_filtered = np.load('../TVB/my_tvb_data/simulations/SEEG_5.npy')
+seegs_not_filtered = (np.squeeze(seegs_not_filtered)).transpose()
 
-## projection matrix and seegs
-proj_mat = np.load(os.path.join(path, 'pyqt_data/seeg_projection_matrix.npy'))
-seegs_not_filtered = np.dot(proj_mat, tavgs)
+
 def butterworth_bandpass(lowcut, highcut, fs, order=5):
     """
     Build a diggital Butterworth filter
 
     """
-    nyq  = 0.5 * fs        # nyquist sampling rate
-    low  = lowcut / nyq    # normalize frequency
+    nyq = 0.5 * fs        # nyquist sampling rate
+    low = lowcut / nyq    # normalize frequency
     high = highcut / nyq   # normalize frequency
     b, a = butter(order, [low, high], btype='band')
     return b, a
@@ -54,15 +59,19 @@ def filter_data(data, lowcut, highcut, fs, order=5):
     return y
 
 fs = 110.0
-lowcut = 0.1   
+lowcut = 0.1
 highcut = 50.0
 seegs = filter_data(seegs_not_filtered, lowcut, highcut, fs, order=6)
 seegs[39] = 0
 
-## electrodes
-positions = np.load(os.path.join(path, 'pyqt_data/positions.npy'))
+# electrodes
+positions = np.loadtxt(os.path.join(path, 'pyqt_data/positions.txt'))
+positions[:, 0] = -positions[:, 0]
+positions[:, 1] = -positions[:, 1]
+
+
 electrodes_color = np.loadtxt(os.path.join(path, 'pyqt_data/electrodes_color.txt'))
-f = open(os.path.join(path, 'pyqt_data/name_electrodes.txt'),'r')
+f = open(os.path.join(path, 'pyqt_data/name_electrodes.txt'), 'r')
 global name_electrodes
 name_electrodes = []
 for line in f:
@@ -72,11 +81,11 @@ f.close()
 print('data loaded')
 
 
-## QT application
+# QT application
 app = QtGui.QApplication([])
 mw = QtGui.QMainWindow()
 mw.setWindowTitle('region simulation')
-mw.resize(1000,800)
+mw.resize(1000, 800)
 cw = QtGui.QWidget()
 mw.setCentralWidget(cw)
 l = QtGui.QVBoxLayout()
@@ -84,53 +93,60 @@ l = QtGui.QGridLayout()
 cw.setLayout(l)
 
 
-## first window
-pw1 = pg.PlotWidget(name='SEEG')  
-l.addWidget(pw1,0,0)
+# first window
+pw1 = pg.PlotWidget(name='SEEG_3')
+l.addWidget(pw1, 0, 0)
 for i in range(seegs.shape[0]):
     #st = (seegs[i,:]-np.min(seegs[i,:])) / (np.max(seegs[i,:])-np.min(seegs[i,:]))
-    st = seegs[i,:]
+    st = seegs[i, :]
     pw1.plot(1000*st+i, pen=electrodes_color[i])
-lr = pg.LinearRegionItem([4000,8000])
-lr2 = pg.LinearRegionItem([4000,8000])
+lr = pg.LinearRegionItem([4000, 8000])
+lr2 = pg.LinearRegionItem([4000, 8000])
 lr.setZValue(-10)
+
+
 def updatePlot1():
     indx1, indx2 = lr2.getRegion()
     indx1, indx2 = indx1, indx2
-    lr.setRegion([indx1,indx2])
+    lr.setRegion([indx1, indx2])
 lr2.sigRegionChanged.connect(updatePlot1)
 updatePlot1()
 pw1.addItem(lr)
 
-## second window
-pw2 = pg.PlotWidget(name='Plot2', title='TAVG')
-l.addWidget(pw2,1,0)
+# second window
+pw2 = pg.PlotWidget(name='Plot2', title='TAVG_3')
+l.addWidget(pw2, 1, 0)
 pw2.setXLink(pw1)
 for i in range(tavgs.shape[0]):
-    #st = (tavgs[i,:]-np.min(tavgs[i,:])) / (np.max(tavgs[i,:])-np.min(tavgs[i,:]))
-    st = tavgs[i,:]
-    pw2.plot(st+i, pen=(0,0,255,200))
+    #st = (tavgs[i, :]-np.min(tavgs[i, :])) / (np.max(tavgs[i, :])-np.min(tavgs[i, :]))
+    st = tavgs[i, :]
+    pw2.plot(st+i, pen=(0, 0, 255, 200))
 lr2.setZValue(-10)
+
+
 def updatePlot2():
     indx1, indx2 = lr.getRegion()
     indx1, indx2 = indx1, indx2
-    lr2.setRegion([indx1,indx2])
+    lr2.setRegion([indx1, indx2])
 lr.sigRegionChanged.connect(updatePlot2)
 updatePlot2()
 pw2.addItem(lr2)
 
+
 # third window
 pw3 = gl.GLViewWidget()
-l.addWidget(pw3, 0,1)
-pw3.setCameraPosition(distance=120, azimuth=-210)
+l.addWidget(pw3, 0, 1)
+pw3.setCameraPosition(distance=200, azimuth=-210)
 mean_verts = np.mean(verts, axis=0)
 max_verts = np.max(verts, axis=0)*0.01
-verts = -(verts - mean_verts)/max_verts
+# verts = -(verts - mean_verts)/max_verts
 surf_nf = faces.shape[0]
 surf_nv = verts.shape[0]
 
-surf_item = gl.GLMeshItem(vertexes=verts[:], faces=faces[:], 
-                  drawFaces=True, drawEdges=False, color=(32,32,32,0.5), smooth=True,  shader='shaded')#glOptions='additive', antialias=True)
+surf_item = gl.GLMeshItem(vertexes=verts[:], faces=faces[:],
+                  drawFaces=True, drawEdges=False, color=(32, 32, 32, 0.5),
+                          smooth=True,  shader='shaded')
+# glOptions='additive', antialias=True)
 
 pw3.addItem(surf_item)
 
@@ -139,7 +155,8 @@ seeg_item = []
 for i in range(seegs.shape[0]):
     seeg_data.append(gl.MeshData.sphere(rows=10, cols=10, radius=1.))
     seeg_item.append(gl.GLMeshItem(meshdata=seeg_data[i], smooth=True, shader='shaded', glOptions='additive'))
-    seeg_item[i].translate(-(positions[i,0]-mean_verts[0])/max_verts[0] , -(positions[i,1]-mean_verts[1])/max_verts[1] , -(positions[i,2]-mean_verts[2])/max_verts[2] )
+    #seeg_item[i].translate(-(positions[i,0]-mean_verts[0])/max_verts[0] , -(positions[i,1]-mean_verts[1])/max_verts[1] , -(positions[i,2]-mean_verts[2])/max_verts[2] )
+    seeg_item[i].translate(-positions[i,0], -positions[i,1], -positions[i,2] )
     pw3.addItem(seeg_item[i])
     seeg_item[i].setColor(electrodes_color[i]/255.)
 
@@ -148,7 +165,8 @@ centres_item1 = []
 for i in range(centres.shape[0]):
     centres_data1.append(gl.MeshData.sphere(rows=10, cols=10, radius=1.))
     centres_item1.append(gl.GLMeshItem(meshdata=centres_data1[i], smooth=True, color=(1, 0, 0, 1), shader='shaded', glOptions='additive'))
-    centres_item1[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+    #centres_item1[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+    centres_item1[i].translate(centres[i,0], centres[i,1] , centres[i,2])
     pw3.addItem(centres_item1[i])
 
 
@@ -157,18 +175,21 @@ def updatePlot3():
     indx1, indx2 = indx1, indx2
     tavgs_factor = 0.03
     tavgs_offset = 1
+
     for i in range(centres.shape[0]):
         ts_tavgs = np.sum(np.abs(tavgs[i, int(indx1):int(indx1)+100] - np.mean(tavgs[i, int(indx1):int(indx1)+100])))
         centres_item1[i].resetTransform()
-        centres_item1[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+        #centres_item1[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+        centres_item1[i].translate(centres[i,0], centres[i,1] , centres[i,2])
         centres_item1[i].scale(tavgs_offset + tavgs_factor*ts_tavgs, tavgs_offset + tavgs_factor*ts_tavgs, tavgs_offset + tavgs_factor*ts_tavgs)
         centres_item1[i].meshDataChanged()
 
-    seegs_factor = 100
+    seegs_factor = 10
     for i in range(seegs.shape[0]):
         ts_seegs = np.sum(np.abs(seegs[i, int(indx1):int(indx1)+100] - np.mean(seegs[i, int(indx1):int(indx1)+100])))
         seeg_item[i].resetTransform()
-        seeg_item[i].translate(-(positions[i,0]-mean_verts[0])/max_verts[0] , -(positions[i,1]-mean_verts[1])/max_verts[1] , -(positions[i,2]-mean_verts[2])/max_verts[2] )
+        #seeg_item[i].translate(-(positions[i,0]-mean_verts[0])/max_verts[0] , -(positions[i,1]-mean_verts[1])/max_verts[1] , -(positions[i,2]-mean_verts[2])/max_verts[2] )
+        seeg_item[i].translate(positions[i,0], positions[i,1], positions[i,2] )
         seeg_item[i].scale(1+seegs_factor*ts_seegs, 1+seegs_factor*ts_seegs, 1+seegs_factor*ts_seegs)
         seeg_item[i].meshDataChanged()
 
@@ -179,7 +200,7 @@ updatePlot3()
 pw4 = gl.GLViewWidget()
 l.addWidget(pw4, 1,1)
 
-pw4.setCameraPosition(distance=120, azimuth=-210)
+pw4.setCameraPosition(distance=200, azimuth=-210)
 
 
 vertcolors = np.ones((surf_nv, 4)) * np.array([0.7,0.67,0.6,0])
@@ -192,7 +213,8 @@ centres_item = []
 for i in range(centres.shape[0]):
     centres_data.append(gl.MeshData.sphere(rows=10, cols=10, radius=1.))
     centres_item.append(gl.GLMeshItem(meshdata=centres_data[i], smooth=True, color=(1, 0, 0, 1), shader='shaded', glOptions='additive'))
-    centres_item[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+    #centres_item[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+    centres_item[i].translate(centres[i,0], centres[i,1] , centres[i,2])
     pw4.addItem(centres_item[i])
 
 
@@ -205,7 +227,8 @@ def updatePlot4():
     for i in range(centres.shape[0]):
         ts_tavgs = np.sum(np.abs(tavgs[i, int(indx1):int(indx1)+100] - np.mean(tavgs[i, int(indx1):int(indx1)+100])))
         centres_item[i].resetTransform()
-        centres_item[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+        #centres_item[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+        centres_item[i].translate(centres[i,0], centres[i,1] , centres[i,2])
         centres_item[i].scale(tavgs_offset + tavgs_factor*ts_tavgs, tavgs_offset + tavgs_factor*ts_tavgs, tavgs_offset + tavgs_factor*ts_tavgs)
 
         centres_item[i].meshDataChanged()
@@ -214,11 +237,11 @@ def updatePlot4():
 lr.sigRegionChanged.connect(updatePlot4)
 updatePlot4()
 
-## set sizes
+# set sizes
 pw1.sizeHint = pw2.sizeHint = pw3.sizeHint = pw4.sizeHint = lambda: pg.QtCore.QSize(100, 100)
 pw3.setSizePolicy(pw1.sizePolicy())
 pw4.setSizePolicy(pw1.sizePolicy())
 
-## show
+# show
 mw.show()
 QtGui.QApplication.instance().exec_()
--- pyqt_surface_visualizer.py
+++ pyqt_surface_visualizer.py
@@ -4,71 +4,79 @@ from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 from matplotlib import cm
 from scipy.signal import butter, lfilter
-
+import h5py as h5
+import os
 
 print('load_data')
-
+path = '/disk2/Work/Simulations/TVB/'
+path_name = '2014-03-17_09-19'
 ## Surface
-verts = np.load('pyqt_data/verts.npy')
-faces = np.load('pyqt_data/faces.npy')
-centres = np.loadtxt('pyqt_data/centres.txt')
-vertex_mapping = np.load('pyqt_data/vertex_mapping.npy')  
-g = open('pyqt_data/name_regions.txt','r')
+verts = np.load(os.path.join(path, 'pyqt_data/verts.npy'))
+faces = np.load(os.path.join(path, 'pyqt_data/faces.npy'))
+centres = np.loadtxt(os.path.join(path, 'pyqt_data/centres.txt'))
+vertex_mapping = np.load(os.path.join(path, 'pyqt_data/vertex_mapping.npy'))
+region_mapping = np.loadtxt(os.path.join(path, 'pyqt_data/region_mapping.txt'))
+g = open(os.path.join(path, 'pyqt_data/name_regions.txt'),'r')
 global name_regions
 name_regions = []
 for line in g:
     name_regions.append(line)
 g.close()
 
+
 ## TAVG
-x01_curr = 2.5
-x02_curr = 3.0
-num_curr = 4.0
-Ks_curr = 0.5
-x0_curr = 69
 global tavgs
-tavgs = np.load('sEEG_sim/results/compressed_different_'+str(x02_curr)+
-                '_epileptogenic_'+str(x01_curr)+'_net_'
-                +str(num_curr)+'_Ks_'+str(Ks_curr)+'_x0_'+str(x0_curr)+'.npy')
+surf_tavgs_h5 = h5.File(os.path.join(path, 'surface_sim/', path_name + '_TimeSeriesSurface.h5'))
+#surf_tavgs = np.squeeze(-surf_tavgs_h5['data'][:,0,:] + surf_tavgs_h5['data'][:,1,:])
+surf_tavgs = np.squeeze(surf_tavgs_h5['data'][:,0,:])
+tavgs = np.zeros(( int(np.max(region_mapping)), surf_tavgs.shape[0]))
+for i in np.arange(np.max(region_mapping)):
+    tavgs[i] = np.mean(surf_tavgs[..., np.nonzero(region_mapping == i)[0]], axis=1)
 
 ## projection matrix and seegs
-proj_mat = np.load('seeg_projection_matrix.npy')
-seegs_not_filtered = np.dot(proj_mat, tavgs)
-def butterworth_bandpass(lowcut, highcut, fs, order=5):
-    """
-    Build a diggital Butterworth filter
-
-    """
-    nyq  = 0.5 * fs        # nyquist sampling rate
-    low  = lowcut / nyq    # normalize frequency
-    high = highcut / nyq   # normalize frequency
-    b, a = butter(order, [low, high], btype='band')
-    return b, a
-
-
-def filter_data(data, lowcut, highcut, fs, order=5):
-    # get filter coefficients
-    b, a = butterworth_bandpass(lowcut, highcut, fs, order=order)
-    # filter data
-    y = lfilter(b, a, data)
-    return y
-
-fs = 110.0
-lowcut = 0.1   
-highcut = 50.0
-seegs = filter_data(seegs_not_filtered, lowcut, highcut, fs, order=6)
-seegs[37:41] = 0.
-seegs[52:55] = 0.
+proj_mat = np.load(os.path.join(path, 'pyqt_data/seeg_projection_matrix.npy'))
+seegs_not_filtered_h5 = h5.File(os.path.join(path, 'surface_sim/', path_name + '_TimeSeriesSEEG.h5'))
+seegs_not_filtered = (np.squeeze(seegs_not_filtered_h5['data'])).transpose()
+seegs = seegs_not_filtered
+#def butterworth_bandpass(lowcut, highcut, fs, order=5):
+#    """
+#    Build a diggital Butterworth filter
+#
+#    """
+#    nyq  = 0.5 * fs        # nyquist sampling rate
+#    low  = lowcut / nyq    # normalize frequency
+#    high = highcut / nyq   # normalize frequency
+#    b, a = butter(order, [low, high], btype='band')
+#    return b, a
+#
+#
+#def filter_data(data, lowcut, highcut, fs, order=5):
+#    # get filter coefficients
+#    b, a = butterworth_bandpass(lowcut, highcut, fs, order=order)
+#    # filter data
+#    y = lfilter(b, a, data)
+#    return y
+#
+#fs = 110.0
+#lowcut = 0.1   
+#highcut = 50.0
+#seegs = filter_data(seegs_not_filtered, lowcut, highcut, fs, order=6)
+#seegs[37:41] = 0.
+#seegs[52:55] = 0.
 
 ## electrodes
-positions = np.load('pyqt_data/positions.npy')
-electrodes_color = np.loadtxt('pyqt_data/electrodes_color.txt')
-f = open('pyqt_data/name_electrodes.txt','r')
+positions = np.load(os.path.join(path, 'pyqt_data/positions.npy'))
+electrodes_color = np.loadtxt(os.path.join(path, 'pyqt_data/electrodes_color.txt'))
+f = open(os.path.join(path, 'pyqt_data/name_electrodes.txt'),'r')
 global name_electrodes
 name_electrodes = []
 for line in f:
     name_electrodes.append(line)
 f.close()
+
+## EEG
+eegs_h5= h5.File(os.path.join(path, 'surface_sim/', path_name + '_TimeSeriesSurface.h5'))
+eegs = (np.squeeze(eegs_h5['data'])).transpose()
 
 print('data loaded')
 
@@ -91,13 +99,13 @@ l.addWidget(pw1,0,0)
 for i in range(seegs.shape[0]):
     #st = (seegs[i,:]-np.min(seegs[i,:])) / (np.max(seegs[i,:])-np.min(seegs[i,:]))
     st = seegs[i,:]
-    pw1.plot(200*st+i, pen=electrodes_color[i])
-lr = pg.LinearRegionItem([4000,8000])
-lr2 = pg.LinearRegionItem([4000,8000])
+    pw1.plot(100*st+i, pen=electrodes_color[i])
+lr = pg.LinearRegionItem([400,800])
+lr2 = pg.LinearRegionItem([400,800])
 lr.setZValue(-10)
 def updatePlot1():
     indx1, indx2 = lr2.getRegion()
-    indx1, indx2 = indx1, indx2
+    indx1, indx2 = indx1*10., indx2*10.
     lr.setRegion([indx1,indx2])
 lr2.sigRegionChanged.connect(updatePlot1)
 updatePlot1()
@@ -106,7 +114,7 @@ pw1.addItem(lr)
 ## second window
 pw2 = pg.PlotWidget(name='Plot2', title='TAVG')
 l.addWidget(pw2,1,0)
-pw2.setXLink(pw1)
+#pw2.setXLink(pw1)
 for i in range(tavgs.shape[0]):
     #st = (tavgs[i,:]-np.min(tavgs[i,:])) / (np.max(tavgs[i,:])-np.min(tavgs[i,:]))
     st = tavgs[i,:]
@@ -114,7 +122,7 @@ for i in range(tavgs.shape[0]):
 lr2.setZValue(-10)
 def updatePlot2():
     indx1, indx2 = lr.getRegion()
-    indx1, indx2 = indx1, indx2
+    indx1, indx2 = indx1/10., indx2/10.
     lr2.setRegion([indx1,indx2])
 lr.sigRegionChanged.connect(updatePlot2)
 updatePlot2()
@@ -144,29 +152,33 @@ for i in range(seegs.shape[0]):
     pw3.addItem(seeg_item[i])
     seeg_item[i].setColor(electrodes_color[i]/255.)
 
-centres_data1 = []
-centres_item1 = []
-for i in range(centres.shape[0]):
-    centres_data1.append(gl.MeshData.sphere(rows=10, cols=10, radius=1.))
-    centres_item1.append(gl.GLMeshItem(meshdata=centres_data1[i], smooth=True, color=(1, 0, 0, 1), shader='shaded', glOptions='additive'))
-    centres_item1[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
-    pw3.addItem(centres_item1[i])
+#centres_data1 = []
+#centres_item1 = []
+#for i in range(centres.shape[0]):
+#    centres_data1.append(gl.MeshData.sphere(rows=10, cols=10, radius=1.))
+#    centres_item1.append(gl.GLMeshItem(meshdata=centres_data1[i], smooth=True, color=(1, 0, 0, 1), shader='shaded', glOptions='additive'))
+#    centres_item1[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+#    pw3.addItem(centres_item1[i])
 
 
 def updatePlot3():
     indx1, indx2 = lr.getRegion()
     indx1, indx2 = indx1, indx2
-
-    for i in range(centres.shape[0]):
-        centres_item1[i].resetTransform()
-        centres_item1[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
-        centres_item1[i].scale(2*np.abs(tavgs[i, int(indx1)]), 2*np.abs(tavgs[i, int(indx1)]), 2*np.abs(tavgs[i, int(indx1)]))
-        centres_item1[i].meshDataChanged()
-
+#    tavgs_factor = 0.03
+#    tavgs_offset = 1
+#    for i in range(centres.shape[0]):
+#        ts_tavgs = np.sum(np.abs(tavgs[i, int(indx1):int(indx1)+100] - np.mean(tavgs[i, int(indx1):int(indx1)+100])))
+#        centres_item1[i].resetTransform()
+#        centres_item1[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+#        centres_item1[i].scale(tavgs_offset + tavgs_factor*ts_tavgs, tavgs_offset + tavgs_factor*ts_tavgs, tavgs_offset + tavgs_factor*ts_tavgs)
+#        centres_item1[i].meshDataChanged()
+#
+    seegs_factor = 10
     for i in range(seegs.shape[0]):
+        ts_seegs = np.sum(np.abs(seegs[i, int(indx1):int(indx1)+10] - np.mean(seegs[i, int(indx1):int(indx1)+10])))
         seeg_item[i].resetTransform()
         seeg_item[i].translate(-(positions[i,0]-mean_verts[0])/max_verts[0] , -(positions[i,1]-mean_verts[1])/max_verts[1] , -(positions[i,2]-mean_verts[2])/max_verts[2] )
-        seeg_item[i].scale(1+200*np.abs(seegs[i, int(indx1)]), 1+200*np.abs(seegs[i, int(indx1)]), 1+200*np.abs(seegs[i, int(indx1)]))
+        seeg_item[i].scale(1+seegs_factor*ts_seegs, 1+seegs_factor*ts_seegs, 1+seegs_factor*ts_seegs)
         seeg_item[i].meshDataChanged()
 
 lr.sigRegionChanged.connect(updatePlot3)
@@ -184,27 +196,33 @@ surf_data = gl.MeshData(vertexes=verts[:], faces=faces[:])
 m1 = gl.GLMeshItem(meshdata=surf_data, smooth=True, shader='shaded')
 pw4.addItem(m1)
 
-centres_data = []
-centres_item = []
-for i in range(centres.shape[0]):
-    centres_data.append(gl.MeshData.sphere(rows=10, cols=10, radius=1.))
-    centres_item.append(gl.GLMeshItem(meshdata=centres_data[i], smooth=True, color=(1, 0, 0, 1), shader='shaded', glOptions='additive'))
-    centres_item[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
-    pw4.addItem(centres_item[i])
+#centres_data = []
+#centres_item = []
+#for i in range(centres.shape[0]):
+#    centres_data.append(gl.MeshData.sphere(rows=10, cols=10, radius=1.))
+#    centres_item.append(gl.GLMeshItem(meshdata=centres_data[i], smooth=True, color=(1, 0, 0, 1), shader='shaded', glOptions='additive'))
+#    centres_item[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+#    pw4.addItem(centres_item[i])
 
 
 
 def updatePlot4():
     indx1, indx2 = lr.getRegion()
-    indx1, indx2 = indx1, indx2
+    indx1, indx2 = indx1/10., indx2/10.
+#    tavgs_factor = 0.03
+#    tavgs_offset = 1
+#    for i in range(centres.shape[0]):
+#        ts_tavgs = np.sum(np.abs(tavgs[i, int(indx1):int(indx1)+100] - np.mean(tavgs[i, int(indx1):int(indx1)+100])))
+#        centres_item[i].resetTransform()
+#        centres_item[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
+#        centres_item[i].scale(tavgs_offset + tavgs_factor*ts_tavgs, tavgs_offset + tavgs_factor*ts_tavgs, tavgs_offset + tavgs_factor*ts_tavgs)
+#
+#        centres_item[i].meshDataChanged()
 
-    for i in range(centres.shape[0]):
-        centres_item[i].resetTransform()
-        centres_item[i].translate(-(centres[i,0]-mean_verts[0])/max_verts[0] , -(centres[i,1]-mean_verts[1])/max_verts[1] , -(centres[i,2]-mean_verts[2])/max_verts[2] )
-        centres_item[i].scale(2*np.abs(tavgs[i, int(indx1)]), 2*np.abs(tavgs[i, int(indx1)]), 2*np.abs(tavgs[i, int(indx1)]))
-
-        centres_item[i].meshDataChanged()
-
+    colors = cm.jet(np.sum(np.abs(surf_tavgs[int(indx1):int(indx1)+10, :] - np.mean(surf_tavgs[int(indx1):int(indx1)+10, :], axis=0)), axis=0))
+    colors[..., 3] = 0.5
+    surf_data.setVertexColors(colors[:])
+    m1.meshDataChanged()
 
 lr.sigRegionChanged.connect(updatePlot4)
 updatePlot4()
